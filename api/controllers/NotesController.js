38520ee9c15cbb3ed98927d0c4cb1d85afd337ce
marcoguilherme <mgbm13@gmail.com>
2018-05-27 17:17:34 -0300

    Controller code ready

--- api/controllers/NotesController.js
+++ api/controllers/NotesController.js
@@ -1,13 +1,13 @@
 exports.getNotes = function(app, req, res){
     let database = app.config.database;
-    if(database){
-        let NotesModel = new app.api.models.NotesModel(database);
-        NotesModel.allNotes(res);
-    }
+    let NotesModel = new app.api.models.NotesModel(database);
+    NotesModel.allNotes(res);
     
 }
 exports.newNote = function(app, req, res){
-    res.send('Ok')
+    let database = app.config.database;
+    let NotesModel = new app.api.models.NotesModel(database);
+    NotesModel.newNote(res);
 }
 exports.editNote = function(app, req, res){
     res.send('Ok')
